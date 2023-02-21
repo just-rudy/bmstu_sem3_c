9c4a682d521f4f8538a0d93ca01db26460071c0a
Serysheva Daria <1768-sds22u306@users.noreply.git.iu7.bmstu.ru>
2023-02-21 09:00:27 +0300

    Add new file

---
+++ lab_01_02_02/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() //задача 2 вариант 2
+{
+    float xa, ya, xb, yb, xc, yc;
+    printf("Input a(x, y): ");
+    scanf("%f%f", &xa, &ya);
+
+    printf("Input b(x, y): ");
+    scanf("%f%f", &xb, &yb);
+
+    printf("Input c(x, y): ");
+    scanf("%f%f", &xc, &yc);
+
+    float ab = sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb));
+    float ac = sqrt((xa-xc)*(xa-xc) + (ya-yc)*(ya-yc));
+    float cb = sqrt((xc-xb)*(xc-xb) + (yc-yb)*(yc-yb));
+    float perimetr = ((ab+ac+cb)*1000000)/1000000;
+    printf("Perimeter: %f", ab+ac+cb);
+
+    return 0;
+}
