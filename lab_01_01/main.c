df95f224c747cd722215656adca232ee21c39fef
sds22u306 <sds22u306@student.bmstu.ru>
2023-02-23 17:41:47 +0300

    fix 1,2

--- lab_01_01/main.c
+++ lab_01_01/main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int s;
     s = 52;
     printf("Year has %d weeks", s);
-
     return 0;
 }
--- lab_01_02_02/main.c
+++ lab_01_02_02/main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-int main() //задача 2 вариант 2
+int main() // задача 2 вариант 2
 {
-    float xa, ya, xb, yb, xc, yc;
+    double xa, ya, xb, yb, xc, yc;
     printf("Input a(x, y): ");
-    scanf("%f%f", &xa, &ya);
+    scanf("%lf%lf", &xa, &ya);
 
     printf("Input b(x, y): ");
-    scanf("%f%f", &xb, &yb);
+    scanf("%lf%lf", &xb, &yb);
 
     printf("Input c(x, y): ");
-    scanf("%f%f", &xc, &yc);
+    scanf("%lf%lf", &xc, &yc);
 
-    float ab = sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb));
-    float ac = sqrt((xa-xc)*(xa-xc) + (ya-yc)*(ya-yc));
-    float cb = sqrt((xc-xb)*(xc-xb) + (yc-yb)*(yc-yb));
-    float perimetr = ((ab+ac+cb)*1000000)/1000000;
-    printf("Perimeter: %f ", ab+ac+cb);
+    double ab = sqrt((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb));
+    double ac = sqrt((xa - xc) * (xa - xc) + (ya - yc) * (ya - yc));
+    double cb = sqrt((xc - xb) * (xc - xb) + (yc - yb) * (yc - yb));
+    printf("Perimeter: %lf ", ab + ac + cb);
 
     return 0;
 }
