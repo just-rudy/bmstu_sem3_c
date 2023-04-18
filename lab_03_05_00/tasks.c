7aa378eae585883b59798a4a3d40728ad533997b
shaa22u355 <89980778+Ross-the-Moose@users.noreply.github.com>
2023-04-18 23:08:29 +0300

    style

--- lab_03_05_00/tasks.c
+++ lab_03_05_00/tasks.c
@@ -5,7 +5,6 @@ void swap(int *link1, int *link2);
 void reverse_arr(int *simple_arr, int cnt);
 void full_matrix(matrix arr, int rows, int cols, int *simple_arr);
 
-
 int check_if_simple(int element)
 {
     int if_simple = (element < 2) ? 0 : 1;
@@ -32,10 +31,10 @@ int make_arr(matrix arr, int rows, int cols, int *simple_arr, int *cnt)
     }
     status = (*cnt) ? 0 : 1;
     if (status == 0)
-        {
-            reverse_arr(simple_arr, (*cnt));
-            full_matrix(arr, rows, cols, simple_arr);
-        }
+    {
+        reverse_arr(simple_arr, (*cnt));
+        full_matrix(arr, rows, cols, simple_arr);
+    }
     return status;
 }
 
