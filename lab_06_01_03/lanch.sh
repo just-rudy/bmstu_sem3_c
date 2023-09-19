e023b9e3787776146d29bc03a4caf78e98fc2c8d
just-rudy <sds22u306@student.bmstu.ru>
2023-09-19 12:30:48 +0300

    fix

--- lab_06_01_03/lanch.sh
+++ lab_06_01_03/lanch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcc *.c -o app.exe
-./app.exe func_tests/data/neg_19_in.txt 100
+./app.exe func_tests/data/pos_04_in.txt 1e5
--- lab_06_01_03/task.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TASK_H__
-#define __TASK_H__
-
-#include "consts.h"
-
-void proccess(array arr, int n, double price);
-
-#endif
